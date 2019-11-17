376f315f1ece8ee83fc94e4acbf1c2a9a7dca33d
asd147asd147 <ds5vfr@gmail.com>
2019-11-17 16:48:50 +0900

    Java

--- HW03/LeftArrow.java
+++ HW03/LeftArrow.java
@@ -1,16 +1,17 @@
 package hw03;
 
-public class LeftArrow extends ShapeBase {
+public class LeftArrow extends ShapeBase implements ShapeInterface{
 	private int tail;
 	private int width;
+	private int base;
 	
 	public void drawHere() {
-		int arrowhead = getOffset();
+		int arrowhead = base+getOffset();
 		int headjump = 1;
 		for(int i = 0; i < width/2; i++) {
 			space(arrowhead);
 			System.out.print("*");
-			if(arrowhead != getOffset()) {
+			if(arrowhead != base + getOffset()) {
 				space(headjump);
 				System.out.println("*");
 				headjump += 2;
@@ -20,6 +21,7 @@ public class LeftArrow extends ShapeBase {
 			}
 			arrowhead-=2;
 		}
+		space(getOffset());
 		System.out.print("*");
 		space(headjump);
 		for(int i = 0; i < tail; i++) {
@@ -30,7 +32,7 @@ public class LeftArrow extends ShapeBase {
 		for(int i = 0; i < width/2; i++) {
 			space(arrowhead);
 			System.out.print("*");
-			if(arrowhead != getOffset()) {
+			if(arrowhead != base + getOffset()) {
 				headjump -= 2;
 				space(headjump);
 				System.out.println("*");				
@@ -41,38 +43,34 @@ public class LeftArrow extends ShapeBase {
 			arrowhead+=2;
 		}
 	}
-	public LeftArrow() {
-		tail = 5;
-		width = 3;
-		setOffset(width-1);
-	}
-	public LeftArrow(int theTail, int theWidth) {
+
+	public LeftArrow(int theTail, int theWidth, int offset) {
 		if(theWidth>= 3 && theWidth%2 == 1) {
 			tail = theTail;
 			width = theWidth;
-			setOffset(theWidth-1);
+			base = theWidth-1;
+			setOffset(offset);
+		}
+		else {
+			tail = theTail;
+			width = theWidth+1;
+			base = width-1;
+			setOffset(offset);
+			System.out.println("Width is "+theWidth+" -> " + width);
 		}
 	}
-	public void setWidth(int theWidth) {
-		if(theWidth>= 3 && theWidth%2 == 1) {
-			width = theWidth;
-			setOffset(theWidth-1);
-		}
-	}
-	public void setTail(int theTail) {
-		tail = theTail;
-	}
-	
+
 	private void space(int step) {
 		for(int i = 0; i < step; i++)
 			System.out.print(" ");
 	}
 	
 	public static void main(String[] args) {
-		LeftArrow arrow1 = new LeftArrow();
+		ShapeInterface arrow1 = new LeftArrow(5,4,0);
 		arrow1.drawHere();
-		arrow1.setTail(16);
-		arrow1.setWidth(7);
-		arrow1.drawAt(3);
+		ShapeInterface arrow2 = new LeftArrow(16,7,5);
+		arrow2.drawAt(2);
+		ShapeInterface arrow3 = new LeftArrow(10,9,2);
+		arrow3.drawAt(3);
 	}
 }
--- HW03/RightArrow.java
+++ HW03/RightArrow.java
@@ -1,13 +1,14 @@
 package hw03;
 
-public class RightArrow extends ShapeBase {
+public class RightArrow extends ShapeBase implements ShapeInterface {
 	private int tail;
 	private int width;
+	private int base;
 	
 	public void drawHere() {
 		int arrowhead = 0;
 		for(int i = 0; i < width/2; i++) {
-			space(getOffset());
+			space(base + getOffset());
 			System.out.print("*");
 			if(arrowhead != 0) {
 				space(arrowhead);
@@ -19,6 +20,7 @@ public class RightArrow extends ShapeBase {
 			}
 			arrowhead++;
 		}
+		space(getOffset());
 		for(int i = 0; i < tail; ++i) {
 			System.out.print("*");
 		}
@@ -26,7 +28,7 @@ public class RightArrow extends ShapeBase {
 		arrowhead -=2;
 		System.out.println("*");
 		for(int i = 0; i < width/2; i++) {
-			space(getOffset());
+			space(base + getOffset());
 			System.out.print("*");
 			if(arrowhead != -1) {
 				space(arrowhead);
@@ -39,37 +41,34 @@ public class RightArrow extends ShapeBase {
 			arrowhead--;
 		}
 	}
-	public RightArrow() {
-		tail = 5;
-		width = 3;
-		setOffset(tail-1);
-	}
-	public RightArrow(int theTail, int theWidth) {
+
+	public RightArrow(int theTail, int theWidth, int offset) {
 		if(theWidth>= 3 && theWidth%2 == 1) {
 			tail = theTail;
 			width = theWidth;
-			setOffset(theTail-1);
+			base = theTail-1;
+			setOffset(offset);
+		}
+		else {
+			tail = theTail;
+			width = theWidth+1;
+			base = theTail-1;
+			setOffset(offset);
+			System.out.println("Width is "+theWidth+" -> " + width);
 		}
 	}
-	public void setWidth(int theWidth) {
-		if(theWidth>= 3 && theWidth%2 == 1)
-			width = theWidth;
-	}
-	public void setTail(int theTail) {
-		tail = theTail;
-		setOffset(tail-1);
-	}
-	
+
 	private void space(int step) {
 		for(int i = 0; i < step; i++)
 			System.out.print(" ");
 	}
 	
 	public static void main(String[] args) {
-		RightArrow arrow1 = new RightArrow();
+		ShapeInterface arrow1 = new RightArrow(5,4,0);
 		arrow1.drawHere();
-		arrow1.setTail(16);
-		arrow1.setWidth(7);
-		arrow1.drawAt(3);
+		ShapeInterface arrow2 = new RightArrow(16,7,5);
+		arrow2.drawAt(2);
+		ShapeInterface arrow3 = new RightArrow(10,9,2);
+		arrow3.drawAt(3);
 	}
 }
